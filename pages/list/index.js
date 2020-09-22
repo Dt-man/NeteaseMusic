105ce6847086646b32182f01fefc0b30578ac6be
温昊男 <13517370438@163.com>
2020-09-22 18:05:17 +0800

    进度条显示问题，暂停后清除定时器

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,40 +1,71 @@
 // pages/list/index.js
-import {search} from '../../utils/api/search/index'
+import { search } from '../../utils/api/search/index'
 
-import {getMusicUrl} from '../../utils/api/list/index'
+import { getMusicUrl } from '../../utils/api/list/index'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list:[],
+    list: [],
     isPlay: false,
-    progressDuration: 3
+    percent: 0,
+    progressDuration: 300,
+    music: null,
+    musicId: null,
+    musicFile: null,
+    musicCurrentTime: 0
   },
 
   /**
    * 
    */
   handleTap(e) {
-    console.log(e.currentTarget.dataset.id);
+    const {musicId,isPlay,musicFile,music,percent,musicCurrentTime} = this.data
+    if ((musicId && musicId === e.currentTarget.dataset.id) && isPlay) {
+      this.setData({
+        isPlay: false,
+        percent: (musicCurrentTime / (musicFile.br / 1000)) * 100
+      })
+      console.log(musicCurrentTime,(musicCurrentTime / (musicFile.br / 1000)) * 100);
+      return music.pause()
+    }
+
+    if(musicId && !isPlay) {
+      this.setData({
+        isPlay: true,
+        percent: 100
+      })
+      return music.play()
+    }
+
     wx.showLoading({
       title: '加载中...'
     })
-    getMusicUrl({id:e.currentTarget.dataset.id}).then(res => {
-      if(res.status !== 200) {
-       return wx.showToast({
+    this.setData({
+      percent: 0
+    })
+    getMusicUrl({ id: e.currentTarget.dataset.id || musicId }).then(res => {
+      if (res.status !== 200) {
+        return wx.showToast({
           icon: 'none',
           title: '请求失败,请稍后重试',
           duration: 2000
         })
       }
-      const music = wx.createInnerAudioContext()
       music.src = res.url
       music.play()
       this.setData({
         isPlay: true,
+        percent: 100,
+        progressDuration: res.br / 100,
+        musicId: e.currentTarget.dataset.id,
+        musicFile: res
       })
+      setInterval(()=>{
+        this.data.musicCurrentTime ++
+      },1000)
     }).finally(() => {
       wx.hideLoading()
     })
@@ -44,13 +75,22 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const {keywords} = options
-    search({keywords}).then(res => {
+    const { keywords } = options
+    search({ keywords }).then(res => {
       res.result.songs.forEach(item => {
-        item.newName = item.name.replace(new RegExp(keywords,'gi'),`<span style="color:#ea5455">${keywords}</span>`)
+        item.newName = item.name.replace(new RegExp(keywords, 'gi'), `<span style="color:#ea5455">${keywords}</span>`)
+        item.artists.map((v, i) => {
+          if (!item.artistsName) {
+            return item.artistsName = v.name
+          }
+          if (i <= item.artists.length) {
+            item.artistsName += '、' + v.name
+          }
+        })
       })
       this.setData({
-        list: res.result.songs
+        list: res.result.songs,
+        music: wx.createInnerAudioContext()
       })
     })
   },
